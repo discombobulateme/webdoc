7fdf540299562fb9e26e5ae964bcc2ec903be110
discombobulateme <p@discombobulate.me>
2020-07-28 10:54:54 +0200

    delete main after hard reset & created right index

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
-console.log('just to stage')
+/*
+const paloma = {
+  name: 'Paloma',
+  age: 37,
+  email: 'p@p.com',
+  password: '123456',
+  baseStart: 2009,
+  instructor: 'Ruy Fernandes',
+  places: [],
+}
+*/
+
+class Jumpers {
+  constructor(name, age, email, password, baseStart, instructor, places) {
+    this.name = name
+    this.age = age
+    this.email = email
+    this.password = password
+    this.baseStart = baseStart
+    this.instructor = instructor
+    this.places = places
+    this.jumps = []
+  }
+
+  addJump(jump) {
+    this.jumps.push(jump)
+  }
+
+  addDiary(diary) {
+    console.log(`I've just jumped out of ${diary}! This is my jump number ${this.jumps}`);
+  }
+}
+
+class Objects {
+  constructor(type, height, landing) {
+    this.type = type
+    this.height = height
+    this.landing = landing
+  }
+
+  addObject(type) {
+    if (type === 'b' || 'a' || 's' || 'e') {
+      return type
+    } else {
+      return 'You must type one of the objects: b as building, a as antenna, s as span or e as earth';
+    }
+  }
+
+}
+
+const paloma = new Jumpers('paloma', 37, 'p@p.com', '123456', 2009, 'Ruy Fernandes', 'Ponde de Resende')
+const onca = new Objects('', 450, 'green grass')
+
+paloma.addJump(200)
+paloma.addDiary('Kjerag') // returning undefined
+onca.addObject('resende')
+
+console.log(paloma.jumps)
+console.log(onca.type)
