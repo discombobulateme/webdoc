f91e73857b00e0bb6e47550778506cd05da2cd29
discombobulateme <p@discombobulate.me>
2020-08-21 18:56:21 +0200

    Add instantiations

--- src/models/index.js
+++ src/models/index.js
@@ -1,25 +1,28 @@
 const Athlete = require('./athlete')
+const Jump = require('./jump')
+const JumpLog = require('./jump-log')
 const Place = require('./place')
-/*
-const paloma = {
-  name: 'Paloma',
-  age: 37,
-  email: 'p@p.com',
-  password: '123456',
-  baseStart: 2009,
-  instructor: 'Ruy Fernandes',
-  places: [],
-}
-*/
+const Supporter = require('./supporter')
 
-const paloma = new Athlete('paloma', 37, 'p@p.com', '123456', 2009, 'Ruy Fernandes', 'Ponde de Resende')
-const kjerag = new Place('e', 450, 'green grass, with cows')
+const exit5 = new Jump('B', 'Kjerag', '15.07.2020', 'Classic Norwegian mountain')
+const jump200 = new JumpLog(exit5, 'This was an amazing jump, a 2 way with Rima')
+const kjerag = new Place(
+  'Lysebotn',
+  [59.034669, 6.5599697],
+  'e',
+  1000,
+  'small gras with lots of rocks or nord sea watr'
+)
+const paloma = new Athlete('paloma', '123', 'p@p.com', ['ruy', 'poncho'], exit5, jump200, kjerag, 37, 'she', 'hello')
+const olav = new Supporter(
+  paloma,
+  kjerag,
+  ['Olav', 'Kristina', 'Stein'],
+  'offers different places to stay, from camping to hotel, food and work exchange among jumpers'
+)
 
-paloma.addJump(203)
-kjerag.addPlaces('Kjerag')
-
-console.log(paloma.jumps)
-console.log(kjerag.type)
-
-// setter test not allowing to overwrite diary property
-// paloma.diary = 'Uhu'
+console.log(exit5)
+console.log(jump200)
+console.log(kjerag)
+console.log(paloma)
+console.log(olav)
