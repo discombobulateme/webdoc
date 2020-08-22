ce739ac5c7be35f959cac70188933ff59b1506fe
discombobulateme <p@discombobulate.me>
2020-08-22 14:22:50 +0200

    Add type to enum

--- src/models/place.js
+++ src/models/place.js
@@ -5,8 +5,10 @@ const placeSchema = new mongoose.Schema({
   coordinates: [Number, Number],
   // Remember to update also im Jump, if change is needed
   modality: {
+    type: String,
     enum: ['B', 'A', 'S', 'E', 'b', 'a', 's', 'e'],
     message: 'Status is required.',
+    required: true,
   },
   height: Number,
   landing: String,
