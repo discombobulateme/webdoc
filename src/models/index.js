c719e0e7967e65a3de509a8defb95e7864f71ea5
discombobulateme <p@discombobulate.me>
2020-08-22 14:21:08 +0200

    Change instantiation syntax

--- src/models/index.js
+++ src/models/index.js
@@ -4,16 +4,24 @@ const JumpLog = require('./jump-log')
 const Place = require('./place')
 const Supporter = require('./supporter')
 
-const exit5 = new Jump('B', 'Kjerag', new Date(), 'Classic Norwegian mountain')
-const jump200 = new JumpLog(exit5, 'This was an amazing jump, a 2 way with Rima')
+Jump.create({
+  modality: 'B',
+  athletes: [],
+  place: 'Kjerag',
+  date: new Date(),
+  description: 'Classic Norwegian mountain',
+  logs: [],
+})
+
+const jump200 = new JumpLog(Jump, 'This was an amazing jump, a 2 way with Rima')
 const kjerag = new Place(
   'Lysebotn',
   [59.034669, 6.5599697],
   'e',
   1000,
-  'small gras with lots of rocks or nord sea watr'
+  'small gras with lots of rocks or nord sea water'
 )
-const paloma = new Athlete('paloma', '123', 'p@p.com', ['ruy', 'poncho'], exit5, jump200, kjerag, 37, 'she', 'hello')
+const paloma = new Athlete('paloma', '123', 'p@p.com', ['ruy', 'poncho'], Jump, jump200, kjerag, 37, 'she', 'hello')
 const olav = new Supporter(
   paloma,
   kjerag,
@@ -21,7 +29,7 @@ const olav = new Supporter(
   'offers different places to stay, from camping to hotel, food and work exchange among jumpers'
 )
 
-console.log(exit5)
+console.log(Jump)
 console.log(jump200)
 console.log(kjerag)
 console.log(paloma)
