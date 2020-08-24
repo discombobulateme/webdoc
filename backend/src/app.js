668483b2151ff5740558f76b83cddb09712c5dad
discombobulateme <p@discombobulate.me>
2020-08-24 19:52:57 +0200

    Move files to backend and add vue frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,8 +5,7 @@ const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 
 const indexRouter = require('./routes/index')
-const usersRouter = require('./routes/users')
-const registerRouter = require('./routes/register')
+const athletesRouter = require('./routes/athletes')
 
 const app = express()
 
@@ -21,16 +20,15 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
-app.use('/users', usersRouter)
-app.use('/register', registerRouter)
+app.use('/athletes', athletesRouter)
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   next(createError(404))
 })
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
