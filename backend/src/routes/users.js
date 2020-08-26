e463249fadfce65a0eeb82102e1a9a4cbb3846cf
discombobulateme <p@discombobulate.me>
2020-08-26 16:49:39 +0200

    Add next to get route

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,21 +1,40 @@
-var express = require('express')
-var router = express.Router()
-var users = [
-  { name: 'palomaaa', age: 15 },
-  { name: 'gabyyy', age: 65 },
-  { name: 'alina', age: 8 },
-  { name: 'hilmi', age: 188 },
-  { name: 'jing', age: 1 },
-]
+const express = require('express')
+
+const router = express.Router()
+
+const User = require('../models/user')
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send(users)
+router.get('/', async (req, res) => {
+  const query = {}
+
+  if (req.query.name) {
+    query.name = req.query.name
+  }
+
+  if (req.query.age) {
+    query.age = req.query.age
+  }
+
+  res.send(await User.find(query))
 })
 
-// http://localhost:3000/users/paloma
-router.get('/:userId', function (req, res, next) {
-  res.send(users[req.params.userId])
+/* POST create a user */
+router.post('/', async (req, res) => {
+  const createdUser = await User.create(req.body)
+  res.send(createdUser)
+})
+
+router.get('/:userId', async (req, res) => {
+  const user = await User.findById(req.params.userId)
+
+  if (user) res.render('user', { user })
+  else res.sendStatus(404)
+})
+
+router.get('/:userId/json', async (req, res) => {
+  const user = await User.findById(req.params.userId)
+  res.send(user)
 })
 
 module.exports = router
