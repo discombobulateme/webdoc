fd1791e6ed607eff60ac3fe0b673b760ba188f58
discombobulateme <p@discombobulate.me>
2020-09-06 21:18:10 +0200

    Delete console.log first used as test

--- backend/src/routes/athletes.js
+++ backend/src/routes/athletes.js
@@ -12,8 +12,6 @@ router.get('/', async (req, res) => {
 })
 
 router.get('/initialize', async (req, res) => {
-  console.log('***** initializing *****')
-
   const palomaObj = { name: 'paloma', password: '123', email: 'p@p.com', age: 37 }
   const kimObj = { name: 'kim', password: '123', email: 'k@k.com', age: 40 }
   const juliaObj = { name: 'julia', password: '123', email: 'j@j.com', age: 35 }
