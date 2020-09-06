0504ae15847c091fb202e850829abb47a21d7fd1
discombobulateme <p@discombobulate.me>
2020-09-06 21:20:46 +0200

    Delete instantiation of the method inside class

--- backend/src/routes/athletes.js
+++ backend/src/routes/athletes.js
@@ -16,7 +16,6 @@ router.get('/initialize', async (req, res) => {
   const kim = await Athlete.create({ name: 'kim', password: '123', email: 'k@k.com', age: 40 })
   const julia = await Athlete.create({ name: 'julia', password: '123', email: 'j@j.com', age: 35 })
 
-  const palomasJump = await paloma.jump('E', 'Kjerag')
 
   await kim.jump(palomasJump)
   await julia.jump(palomasJump)
