678cffa475200c6a3fd27ca32afee1dbdbabfbe4
discombobulateme <p@discombobulate.me>
2020-09-12 16:25:44 +0200

    Set socket services

--- backend/src/app.js
+++ backend/src/app.js
@@ -41,6 +41,7 @@ if (app.get('env') == 'development') {
 // it does that by proxy, which we need to trust
 app.set('trust proxy', 1)
 
+app.set('io', socketService)
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
