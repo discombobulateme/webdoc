d1d37a32ae8420e3474e96461bee8f29d6dff0f5
discombobulateme <p@discombobulate.me>
2020-09-12 16:25:22 +0200

    Add socket service

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,6 +11,7 @@ const cors = require('cors')
 const Athlete = require('./models/athlete')
 
 const mongooseConnection = require('./database-connection')
+const socketService = require('./socket-service')
 
 const indexRouter = require('./routes/index')
 const athletesRouter = require('./routes/athletes')
