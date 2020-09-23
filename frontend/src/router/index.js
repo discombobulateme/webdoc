695f6be518a88bb407367daf5ba3a81162475649
discombobulateme <p@discombobulate.me>
2020-09-23 23:01:40 +0200

    Add paths boilerplate

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,29 +1,76 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/login.vue'
+import Register from '../views/register.vue'
+// import Account from '../views/account.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
-
-export default router
+export default store => {
+  return new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+      {
+        path: '/',
+        name: 'Home',
+        component: Home
+      },
+      // {
+      //   path: '/athletes',
+      //   name: 'Athletes',
+      //   component: () => import(/* webpackChunkName: "about" */ '../views/athletes.vue')
+      // },
+      // {
+      //   path: '/jumps',
+      //   name: 'Jumps',
+      //   // which is lazy-loaded when the route is visited.
+      //   component: () => import(/* webpackChunkName: "about" */ '../views/jumps.vue')
+      // },
+      // {
+      //   path: '/athletes/:id',
+      //   name: 'AthleteProfile',
+      //   component: () => import(/* webpackChunkName: "about" */ '../views/athlete-profile.vue')
+      // },
+      // {
+      //   path: '/jumps/:id',
+      //   name: 'JumpProfile',
+      //   component: () => import(/* webpackChunkName: "about" */ '../views/jump-profile.vue')
+      // },
+      {
+        path: '/register',
+        name: 'Register',
+        component: Register,
+        beforeEnter(to, from, next) {
+          if (store.state.account) return next('/')
+          return next()
+        }
+      },
+      {
+        path: '/login',
+        name: 'Login',
+        component: Login,
+        beforeEnter(to, from, next) {
+          if (store.state.account) return next('/')
+          return next()
+        }
+      }
+      // {
+      //   path: '/account',
+      //   name: 'Account',
+      //   component: Account
+      // },
+      // {
+      //   path: '/account/edit-athlete',
+      //   name: 'EditAthlete',
+      //   component: () => import(/* webpackChunkName: "about" */ '../views/edit-athlete.vue')
+      // },
+      // {
+      //   path: '/account/edit-jump',
+      //   name: 'EditJump',
+      //   component: () => import(/* webpackChunkName: "about" */ '../views/edit-jump.vue')
+      // }
+    ]
+  })
+}
