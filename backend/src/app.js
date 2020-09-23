b020bc1005619d40cf493e347529705493fefc16
discombobulateme <p@discombobulate.me>
2020-09-23 11:24:20 +0200

    Add express.static in the app use

--- backend/src/app.js
+++ backend/src/app.js
@@ -54,6 +54,7 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
 // express-session connect-mongo
 app.use(
