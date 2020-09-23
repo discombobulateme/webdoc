dd4b52b56bf48eb8fbb0b158b67736f90609fdb7
discombobulateme <p@discombobulate.me>
2020-09-23 11:39:14 +0200

    Move create athlete to athlete test

--- backend/__test__/app.test.js
+++ backend/__test__/app.test.js
@@ -12,27 +12,3 @@ describe('Index', () => {
     expect(response.statusCode).toBe(200)
   })
 })
-
-// Create an athlete
-describe('Athletes endpoints', () => {
-  it.only('post request to /athletes should create an athlete', async () => {
-    const athleteToCreate = {
-      // Date.now garanties the name is unique, as required in model
-      name: `SomeName${Date.now()}`,
-      age: 37,
-      gender: 'Fluid',
-    }
-
-    const createdAthlete = (await request(app).post('/api/athletes').send(athleteToCreate)).body
-    expect(createdAthlete.name).toBe(athleteToCreate.name)
-    expect(createdAthlete.age).toBe(athleteToCreate.age)
-    expect(createdAthlete.gender).toBe(athleteToCreate.gender)
-  })
-
-  it('get request to /athletes should list athletes', async () => {
-    const athleteList = (await request(app).get('/athletes')).body
-    const athletesExist = athleteList.length > 0
-
-    expect(athletesExist).toBe(true)
-  })
-})
