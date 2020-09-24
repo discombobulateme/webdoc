80ac49ce9e5f8179c9875a072a6f70c00ab82378
discombobulateme <p@discombobulate.me>
2020-09-24 22:47:59 +0200

    Add profile and user-detail views

---
+++ frontend/src/views/profile.vue
@@ -0,0 +1,35 @@
+<script>
+import Counter from '@/components/counter.vue'
+import { mapActions, mapState } from 'vuex'
+
+export default {
+  name: 'profile',
+  components: { Counter },
+  data() {
+    return {
+      users: [],
+      time: new Date(),
+      bio: ''
+    }
+  }
+  async created() {
+    this.users = await this.fetchUsers()
+  },
+  methods: {
+    ...mapActions(['fetchUsers']),
+    }
+  },
+  computed: {
+    ...mapState(['user'])
+  }
+}
+</script>
+
+<template lang="pug">
+  .home
+    h1 Webdoc {{ user.name }}
+    p The time is: {{ time }}
+    h2 Users
+    div(v-for="user in users")
+      router-link(:to="`/users/${user._id}`") {{ user.name }}
+</template>
