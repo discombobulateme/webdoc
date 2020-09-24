8ca466c1eaec19ef8b195b6d3522ca78fadfd0ee
discombobulateme <p@discombobulate.me>
2020-09-24 18:41:49 +0200

    Add authentication actions to store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -7,60 +7,39 @@ axios.defaults.withCredentials = true
 
 Vue.use(Vuex)
 
-const mutations = {
-  INCREMENT_COUNT: 'increment count',
-  SET_USER: 'set user'
-}
+const SET_USER = 'set account'
 
 const store = new Vuex.Store({
   state: {
-    count: 0,
     user: null
   },
   mutations: {
-    [mutations.INCREMENT_COUNT](state) {
-      state.count++
-    },
-    [mutations.SET_USER](state, user) {
+    [SET_USER](state, user) {
       state.user = user
     }
   },
   actions: {
-    incrementCount({ commit }) {
-      commit(mutations.INCREMENT_COUNT)
-    },
-    async fetchUser(store, id) {
-      const userRequest = await axios.get(`/api/athletes/${id}`)
-      return userRequest.data
-    },
-    async fetchUsers() {
-      const usersRequest = await axios.get('/api/athletes')
-      return usersRequest.data
-    },
+    // AUTHENTICATION
     async fetchSession({ commit }) {
       const user = await axios.get('/api/account/session')
-      commit(mutations.SET_USER, user.data || null)
+      commit(SET_USER, user.data || null)
     },
     async login({ commit }, credentials) {
-      try {
-        const user = await axios.post('/api/account/session', credentials)
-        commit(mutations.SET_USER, user.data)
-      } catch (e) {
-        throw e
-      }
+      const user = await axios.post('/api/account/session', credentials)
+      commit(SET_USER, user.data)
     },
     async register(store, user) {
       return axios.post('/api/account', user)
     },
     async logout({ commit }) {
       await axios.delete('/api/account/session')
-      commit(mutations.SET_USER, null)
+      commit(SET_USER, null)
     }
   },
   modules: {}
 })
 
-export default async function init() {
+export default async () => {
   await store.dispatch('fetchSession')
   return store
 }
