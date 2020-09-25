60b839ee2a36a6b99afc3d18c67ad7235f1ae7d0
discombobulateme <p@discombobulate.me>
2020-09-25 12:03:00 +0200

    Change text

--- frontend/src/views/profile.vue
+++ frontend/src/views/profile.vue
@@ -26,8 +26,8 @@ export default {
 
 <template lang="pug">
   .home
-    h1 Webdoc {{ user.name }}
-    p The time is: {{ time }}
+    h1 Eu sou {{ user.name }}, e essa é a minha história
+    p Eu me identifico como {{ user.gender }}
     h2 Users
     div(v-for="user in users")
       router-link(:to="`/users/${user._id}`") {{ user.name }}
