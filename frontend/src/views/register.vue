9ce97cae327b71ccaaec3901f6b66eb184414419
discombobulateme <p@discombobulate.me>
2020-09-25 15:50:31 +0200

    Add sunset gradient background

--- frontend/src/views/register.vue
+++ frontend/src/views/register.vue
@@ -58,6 +58,11 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+section {
+  background: rgb(131, 58, 180);
+  background: linear-gradient(90deg, rgba(131, 58, 180, 1) 0%, rgba(253, 29, 29, 1) 50%, rgba(252, 176, 69, 1) 100%);
+}
+
 label {
   display: block;
   margin: 1rem 0;
